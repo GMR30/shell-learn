305c5f6895a734029939f31d8b8835b15c7c10d3
raju2 <raj.sresta@gmail.com>
2024-05-28 20:17:28 +0530

    Listing down the topics and comments added

--- 02.print.sh
+++ 02.print.sh
@@ -1,2 +1,9 @@
 #There are couple of commands to print but echo is the most usable command for print
 echo Hello world
+#syntax echo -e "\e[COLmMESSAGE\e[0m"
+echo -e "\e[31mHello RED Color\e[0m"
+echo -e "\e[32mHello GREEN color\e[0m"
+echo -e "\e[33mYELLOW Color\e[0m"
+echo -e "\e[34mBLUE color\e[0m"
+echo -e "\e[35mMAGENTA color\e[0m"
+echo -e "\e[36mCIYAN color\e[0m"
